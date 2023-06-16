34c3c35aaa9bf84ba22b6544683552aa149c82e6
giuliacimino <giuliacimino00@gmail.com>
2023-06-16 14:38:37 +0200

    aggiunta azienda dto e service

---
+++ src/main/java/it/prova/coge_be/dto/azienda/AziendaDTO.java
@@ -0,0 +1,58 @@
+package it.prova.coge_be.dto.azienda;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotBlank;
+
+import it.prova.coge_be.model.Azienda;
+import it.prova.coge_be.model.Commessa;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AziendaDTO {
+	private Long id;
+	
+	@NotBlank(message = "{ragionesociale.notblank}")
+	private String ragioneSociale;
+	
+	@NotBlank(message = "{partitaIva.notblank}")
+	private String partitaIva;
+	
+	@NotBlank(message = "{indirizzo.notblank}")
+	private String indirizzo;
+	
+	
+	
+	public Azienda buildAziendaModel() {
+		return Azienda.builder().id(this.id).ragioneSociale(this.ragioneSociale).partivaIva(this.partitaIva).indirizzo(this.indirizzo).build();
+	}
+	
+	
+	public static AziendaDTO buildAziendaDTOFromModel(Azienda aziendaModel) {
+		AziendaDTO result= AziendaDTO.builder().id(aziendaModel.getId()).ragioneSociale(aziendaModel.getRagioneSociale()).partitaIva(aziendaModel.getPartivaIva()).indirizzo(aziendaModel.getIndirizzo()).build();
+		return result;
+		
+	}
+	
+	public static List<AziendaDTO> createAziendaDTOFromModelList(List<Azienda> modelListInput){
+		return modelListInput.stream().map(aziendaEntity -> {
+			AziendaDTO result = AziendaDTO.buildAziendaDTOFromModel(aziendaEntity);
+			
+			return result;
+		}).collect(Collectors.toList());
+	}
+	
+	
+	
+
+
+}
---
+++ src/main/java/it/prova/coge_be/service/azienda/AziendaServiceImpl.java
@@ -0,0 +1,49 @@
+package it.prova.coge_be.service.azienda;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.prova.coge_be.model.Azienda;
+import it.prova.coge_be.repository.azienda.AziendaRepository;
+
+public class AziendaServiceImpl implements AziendaService{
+	
+	@Autowired
+	AziendaRepository repository;
+
+	@Override
+	public List<Azienda> listAll() {
+		return (List<Azienda>) repository.findAll();
+
+	}
+
+	@Override
+	public Azienda caricaSingolo(Long id) {
+		return repository.findById(id).orElse(null);
+
+	}
+
+	@Override
+	public Azienda aggiorna(Azienda aziendaInstance) {
+	
+		
+		return repository.save(aziendaInstance);
+	}
+
+	@Override
+	public Azienda inserisciNuovo(Azienda aziendaInstance) {
+		return repository.save(aziendaInstance);
+
+	}
+
+	@Override
+	public void rimuovi(Azienda aziendaInstance) {
+		repository.delete(aziendaInstance);
+
+		
+	}
+	
+	
+
+}
