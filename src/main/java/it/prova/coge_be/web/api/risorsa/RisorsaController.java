dbe96492e280910d37179542891e8efdf8d935f9
simoneriillo <riillosimone@gmail.com>
2023-06-19 13:49:35 +0200

    aggiunto cross origin sui controller

--- src/main/java/it/prova/coge_be/web/api/risorsa/RisorsaController.java
+++ src/main/java/it/prova/coge_be/web/api/risorsa/RisorsaController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,21 +25,22 @@ import it.prova.coge_be.web.api.exception.RisorsaNotFoundException;
 
 @RestController
 @RequestMapping("/api/risorsa")
+@CrossOrigin
 public class RisorsaController {
-	
+
 	@Autowired
 	private RisorsaService service;
-	
+
 	@GetMapping
 	public List<RisorsaDTO> getAll() {
 		return RisorsaDTO.createRisorsaDTOListFromModelList(service.listAllElements(), false, false);
 	}
-	
+
 	@GetMapping("/{id}")
 	public RisorsaDTO getSingleEager(@PathVariable(value = "id", required = true) Long id) {
 		return RisorsaDTO.buildRisorsaDTOFromModel(service.caricaSingoloElementoEager(id), false, true);
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public RisorsaDTO inserisciNuovo(@Valid @RequestBody RisorsaDTO risorsaInstance) {
@@ -47,35 +49,27 @@ public class RisorsaController {
 			throw new IdNotNullForInsertException("impossibile creare un id per la creazione.");
 
 		Risorsa risorsaInserita = service.inserisciNuovo(risorsaInstance.buildModelFromDTO());
-			
+
 		return RisorsaDTO.buildRisorsaDTOFromModel(risorsaInserita, false, false);
 	}
-	
-	
+
 	@PutMapping("/edit/{id}")
-	public RisorsaDTO update(@Valid @RequestBody RisorsaDTO risorsaInput,
-			@PathVariable(required = true) Long id) {
-		
+	public RisorsaDTO update(@Valid @RequestBody RisorsaDTO risorsaInput, @PathVariable(required = true) Long id) {
+
 		risorsaInput.setId(id);
 		Risorsa risorsaAggiornata = service.aggiorna(risorsaInput.buildModelFromDTO());
 		return RisorsaDTO.buildRisorsaDTOFromModel(risorsaAggiornata, false, true);
 	}
-	
+
 	@DeleteMapping("/delete/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable(required = true) Long id) {
 		Risorsa risorsaDaEliminare = service.caricaSingoloElemento(id);
-//
+
 		if (risorsaDaEliminare == null)
 			throw new RisorsaNotFoundException("non è stato trovato alcun dottore.");
 
 		service.rimuoviById(risorsaDaEliminare.getId());
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
